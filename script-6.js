bceeb304e3b360034cd69d18e370cb04c1638157
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-01-12 19:39:53 +0100

    changes in script-6

--- script-6.js
+++ script-6.js
@@ -11,12 +11,44 @@ for (let i= 0; i < arr.length; i++) {
 }
 
 // 2
-const numbs = [1, 5, 4, 10, 0, 3];
-const targetNumber = 4;
-const index = numbs.indexOf(targetNumber);
+let numbs = [1, 5, 4, 10, 0, 3];
+let targetNumber = 4;
+let index = numbs.indexOf(targetNumber);
 
 console.log(`Индекс числа ${targetNumber}: ${index}`);
 
 // 3
+let numbers = [1, 3, 5, 10, 20];
+numbers = numbers.join(' ');
+
+console.log(numbers);
+
+//4
+let rows = 3;
+let columns = 3;
+let multiArray = [];
+
+for (let i = 0; i < rows; i++) {
+    let row = [];
+
+    for (let i = 0; j < columns; j++) {
+        row.push(1);
+    }
+}
+
+
+
+
+
+// 6
+let massiv = [9, 8, 7, 'a', 6, 5];
+
+massiv.sort((a, b) => a - b);
+
+let filteredMassiv = massiv.filter(item => !isNaN(item));
+console.log(filteredMassiv);
+
+
+
 
 
