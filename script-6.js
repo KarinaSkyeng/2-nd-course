6d10224c5060ea731c3c5cff329e6c85c17941e4
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-01-13 21:48:42 +0100

    changes in script-6

--- script-6.js
+++ script-6.js
@@ -24,21 +24,13 @@ numbers = numbers.join(' ');
 console.log(numbers);
 
 //4
-let rows = 3;
-let columns = 3;
-let multiArray = [];
-
-for (let i = 0; i < rows; i++) {
-    let row = [];
-
-    for (let i = 0; j < columns; j++) {
-        row.push(1);
-    }
-}
 
 
+// 5
+let numbs1 = [1, 1, 1];
+numbs1.push('2, 2, 2');
 
-
+console.log(numbs1);
 
 // 6
 let massiv = [9, 8, 7, 'a', 6, 5];
@@ -48,7 +40,52 @@ massiv.sort((a, b) => a - b);
 let filteredMassiv = massiv.filter(item => !isNaN(item));
 console.log(filteredMassiv);
 
+// 7 
+let numbers1 = [9, 8, 7, 6, 5];
 
+    let userNumber = prompt("Угадайте число");
+    if (numbers1.includes(parseInt(userNumber))) {
+        alert("Угадал");
+    } else {
+        alert("Не угадал");
+    }
 
+// 8
+let string = 'abcdef';
+
+let reversedString = string.split('').reverse().join('');
+
+console.log(reversedString);
+
+// 9
+let multiMassiv = [[1, 2, 3,],[4, 5, 6]];
+
+let flatMassiv = multiMassiv.flat();
+
+console.log(flatMassiv);
+
+// 10
+let numbersArray = [2, 8, 4, 7, 1, 6, 9, 3, 5, 10];
+
+for (let i = 0; i < numbersArray.length - 1; i++) {
+
+    let currentElement = numbersArray[i];
+    let nextElement = numbersArray[i + 1];
+
+    let sum = currentElement + nextElement;
+    console.log(`Сумма элементов ${currentElement} и ${nextElement}: ${sum}`);
+}
+
+// 11
+function squareNumbers(numbers) {
+
+    let squares = numbers.map((number) => number ** 2);
+    return squares;
+}
+
+let inputArray = [1, 2, 3, 4, 5];
+let resultArray = squareNumbers(inputArray);
+
+console.log(resultArray);
 
 
